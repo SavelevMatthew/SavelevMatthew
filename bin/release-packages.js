b14725c98ef7777a4d605c4256c35d8d1bffdc3d
Matthew Savelev <savelevmatthew@gmail.com>
2022-12-25 20:41:38 +0500

    ci: added basic publish job

---
+++ bin/release-packages.js
@@ -0,0 +1,11 @@
+const multiRelease = require('multi-semantic-release')
+const path = require('path')
+
+const RELEASE_LIST = [
+    'eslint-config'
+]
+
+const releasePaths = RELEASE_LIST.map(pkgName => path.join(__dirname, '..', 'packages', pkgName, 'package.json'))
+
+multiRelease(releasePaths)
+.then(() => console.log('DONE ✅'))
