6b33323d4635541c2c3cc835f88be8ef4a6f4834
Matthew Savelev <savelevmatthew@gmail.com>
2022-12-25 21:30:47 +0500

    chore: added tsconfig package to release list

--- bin/release-packages.js
+++ bin/release-packages.js
@@ -2,10 +2,11 @@ const multiRelease = require('multi-semantic-release')
 const path = require('path')
 
 const RELEASE_LIST = [
-    'eslint-config'
+    'eslint-config',
+    'tsconfig',
 ]
 
 const releasePaths = RELEASE_LIST.map(pkgName => path.join(__dirname, '..', 'packages', pkgName, 'package.json'))
 
 multiRelease(releasePaths)
-.then(() => console.log('DONE ✅'))
+    .then(() => console.log('DONE ✅'))
